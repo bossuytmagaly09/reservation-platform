9ff32e1618af922d003aadf56d6b10913ca28152
Friedel <mpcnetwork.manager@outlook.com>
2026-01-06 12:04:45 +0100

    nuxt.config geupdated met supabase variabelen

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,20 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
+// nuxt.config.ts
 export default defineNuxtConfig({
+  modules: [
+    '@pinia/nuxt'
+  ],
+  runtimeConfig: {
+    public: {
+      supabaseUrl: process.env.NUXT_PUBLIC_SUPABASE_URL,
+      supabaseAnonKey: process.env.NUXT_PUBLIC_SUPABASE_ANON_KEY
+    }
+  },
+  css: [
+// jouw globale Tailwind / CSS imports
+  ],
+
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true }
 })
+
+
